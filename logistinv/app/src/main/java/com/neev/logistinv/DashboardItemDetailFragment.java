01021b7afb79e73a891026020ae3c4eb83291ea9
unknown <anita.lakshmanan@BDC7-L-5100CS8.GDNINDIA.COM>
2015-10-13 15:47:55 +0530

    Added button for Manage Data to appear on swipe and disappear when clicked. TODO: Fix the item id when clicked (currently if there are less items on the parameter list the item id is not incrementing correctly)

--- logistinv/app/src/main/java/com/neev/logistinv/DashboardItemDetailFragment.java
+++ logistinv/app/src/main/java/com/neev/logistinv/DashboardItemDetailFragment.java
@@ -1,11 +1,20 @@
 package com.neev.logistinv;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.neev.example.R;
 import com.neev.logistinv.dummy.DummyContent;
@@ -16,19 +25,25 @@ import com.neev.logistinv.dummy.DummyContent;
  * in two-pane mode (on tablets) or a {@link DashboardItemDetailActivity}
  * on handsets.
  */
-public class DashboardItemDetailFragment extends Fragment {
+public class DashboardItemDetailFragment extends Fragment
+        implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
-    public static final String ARG_PANE = "today";
+    public static String ARG_PANE = "today";
+    private GestureDetectorCompat mDetector;
 
     /**
      * The dummy content this fragment is presenting.
      */
     private DummyContent.DummyItem mItem;
-
+    /**
+     * The pane in which this fragment is presenting.
+     */
+    private String mPane;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -60,13 +75,90 @@ public class DashboardItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mDetector = new GestureDetectorCompat(getActivity(), this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+        // Detect touched area
         View rootView = inflater.inflate(R.layout.fragment_dashboarditem_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.dashboarditem_detail)).setText(mItem.content);
         }
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                  // ... Respond to touch events
+                return mDetector.onTouchEvent(event);
+
+            }
+        });
 
         return rootView;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.BOTTOM|Gravity.CENTER;
+        final Button btn = new Button(getActivity());
+        btn.setText("MANAGE DATA");
+        btn.setBackgroundColor(Color.RED);
+        final FrameLayout fl = (FrameLayout) getView().getParent();
+        fl.addView(btn, params);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),
+                        "Button clicked index = " + mItem.content, Toast.LENGTH_SHORT)
+                        .show();
+                fl.removeView(btn);
+                //Start Manage Data activity here.
+                //add code for finding which item is displaying currently
+                //fix color and position of button
+            }
+        });
+        return false;
     }
 }
